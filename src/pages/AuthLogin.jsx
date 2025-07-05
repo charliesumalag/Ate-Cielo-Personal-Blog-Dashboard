057e819c1b8a7969fe51600a27d4fa2add9a61ed
Charlie Sumalag <charliesumalag444@yahoo.com.ph>
2025-07-05 16:54:32 +0800

    Login page refine'

--- src/pages/AuthLogin.jsx
+++ src/pages/AuthLogin.jsx
@@ -6,7 +6,7 @@ const AuthLogin = () => {
 
   return (
     <div className='flex flex-col justify-center items-center h-screen font-roboto '>
-        <div className='w-[350px]'>
+        <div className='w-[380px]'>
             <div className='mb-10 flex flex-col gap-2'>
                 <h2 className='text-4xl font-bold tracking-wide text-[#013220]'>Login</h2>
                 <p>Hi, Welcome back admin 👏</p>
@@ -18,7 +18,7 @@ const AuthLogin = () => {
                 </div>
                 <div className='flex flex-col gap-1'>
                     <label htmlFor="">Password</label>
-                    <input type="text" placeholder='Enter your password' className='p-2 border border-gray-300 font-roboto rounded-md outline-0' />
+                    <input type="password" placeholder='Enter your password' className='p-2 border border-gray-300 font-roboto rounded-md outline-0' />
                 </div>
                 <div className='flex justify-between'>
                     <div className='flex flex-row-reverse gap-1 justify-center'>
@@ -29,9 +29,9 @@ const AuthLogin = () => {
                 </div>
                 <button className='bg-[#013220] font-roboto tracking-widest text-white p-2 mt-6 rounded-md cursor-pointer'>Login</button>
                 <p className='text-gray-500' onMouseEnter={() => setIsHover(true)} onMouseLeave={() => setIsHover(false)}>Not registered yet?
-                    <span className='text-[#013220] cursor-pointer'>Create an account</span>
+                    <span className='text-[#013220] ml-1 cursor-pointer'>Create an account</span>
                     <span>
-                        <i className={isHover ? 'fa-solid fa-arrow-up rotate-45 font-extralight text-xs pb-0.5 pr-3 ' : 'fa-solid fa-arrow-up rotate-45 font-extralight text-xs pb-0.5 pr-2 '}></i>
+                        <i className={isHover ? 'fa-solid fa-arrow-up cursor-pointer rotate-45 text-[#013220] font-extralight text-xs pb-1.5 pr-3 ' : 'fa-solid fa-arrow-up rotate-45 cursor-pointer font-extralight text-[#013220] text-xs pb-1.5 pr-2 '}></i>
                     </span>
                 </p>
             </form>
